dba3742d8a2d3dfdbcee99580e00f58cbf4c66f9
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2023-08-25 20:45:31 -0600

    First approach with graphics

---
+++ main.cpp
@@ -0,0 +1,53 @@
+// Universidad del Valle de Guatemala
+// Computacion Paralela y distribuida
+// Proyecto 1
+// Cristian Aguirre   20361
+// Paola de Leon      20308
+// Marco Jurado       20231
+
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+int main()
+{
+    // Inicialización para números aleatorios
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    // Crear una ventana a pantalla completa
+    sf::VideoMode vm = sf::VideoMode::getDesktopMode();
+    sf::RenderWindow window(vm, "Simple Circle", sf::Style::Fullscreen);
+
+    // Inicializar el círculo
+    sf::CircleShape circle;
+    float radius = 50; // Un radio de tamaño fijo, por ejemplo 50
+    circle.setRadius(radius);
+    circle.setFillColor(sf::Color::Green); // Color fijo, por ejemplo verde
+
+    // Posición aleatoria
+    circle.setPosition(std::rand() % (static_cast<int>(vm.width) - static_cast<int>(radius * 2)),
+                       std::rand() % (static_cast<int>(vm.height) - static_cast<int>(radius * 2)));
+
+    while (window.isOpen())
+    {
+        // Procesar eventos
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed ||
+                event.type == sf::Event::KeyPressed ||
+                event.type == sf::Event::MouseButtonPressed)
+            {
+                window.close();
+            }
+        }
+
+        // Dibujar el círculo
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
+
+    return 0;
+}
