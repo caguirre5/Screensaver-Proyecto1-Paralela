2aab185a5ac9904ad5602f2674a07b12932e4197
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2023-09-01 22:10:34 -0600

    Screen saver working

--- main.cpp
+++ main.cpp
@@ -8,9 +8,29 @@
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
-int main()
+struct MovingCircle
 {
+    sf::CircleShape shape;
+    sf::Vector2f velocity;
+};
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Uso: " << argv[0] << " [Numero de circulos]\n";
+        return 1;
+    }
+
+    int NUM_CIRCLES = std::stoi(argv[1]);
+    if (NUM_CIRCLES <= 0)
+    {
+        std::cout << "Por favor, introduce un numero de circulos positivo.\n";
+        return 1;
+    }
+
     // Inicialización para números aleatorios
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
@@ -18,17 +38,17 @@ int main()
     sf::VideoMode vm = sf::VideoMode::getDesktopMode();
     sf::RenderWindow window(vm, "Screen Saver", sf::Style::Fullscreen);
 
-    const int NUM_CIRCLES = 10;
-    sf::CircleShape circles[NUM_CIRCLES];
+    MovingCircle circles[NUM_CIRCLES];
 
-    // Inicializar los círculos con tamaños y colores aleatorios
+    // Inicializar los círculos
     for (int i = 0; i < NUM_CIRCLES; ++i)
     {
         float radius = std::rand() % 100 + 10;
-        circles[i].setRadius(radius);
-        circles[i].setFillColor(sf::Color(std::rand() % 256, std::rand() % 256, std::rand() % 256));
-        circles[i].setPosition(std::rand() % (static_cast<int>(vm.width) - static_cast<int>(radius * 2)),
-                               std::rand() % (static_cast<int>(vm.height) - static_cast<int>(radius * 2)));
+        circles[i].shape.setRadius(radius);
+        circles[i].shape.setFillColor(sf::Color(std::rand() % 256, std::rand() % 256, std::rand() % 256));
+        circles[i].shape.setPosition(std::rand() % (static_cast<int>(vm.width) - static_cast<int>(radius * 2)),
+                                     std::rand() % (static_cast<int>(vm.height) - static_cast<int>(radius * 2)));
+        circles[i].velocity = sf::Vector2f(std::rand() % 200 - 100, std::rand() % 200 - 100);
     }
 
     while (window.isOpen())
@@ -45,11 +65,30 @@ int main()
             }
         }
 
+        // Mover y comprobar colisiones para cada círculo
+        for (int i = 0; i < NUM_CIRCLES; ++i)
+        {
+            sf::Vector2f pos = circles[i].shape.getPosition();
+            sf::Vector2f &vel = circles[i].velocity;
+            float radius = circles[i].shape.getRadius();
+
+            if (pos.x <= 0 || pos.x + 2 * radius >= vm.width)
+            {
+                vel.x = -vel.x;
+            }
+            if (pos.y <= 0 || pos.y + 2 * radius >= vm.height)
+            {
+                vel.y = -vel.y;
+            }
+
+            circles[i].shape.move(vel * 0.05f);
+        }
+
         // Dibujar todo
         window.clear();
         for (int i = 0; i < NUM_CIRCLES; ++i)
         {
-            window.draw(circles[i]);
+            window.draw(circles[i].shape);
         }
         window.display();
     }
